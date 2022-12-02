901b02e18a66c52717083f095dcb42b7045c3475
jake <mail@jakedowns.com>
2022-12-01 18:08:04 -0800

    update filepicker asset

--- Assets/JakeDowns/Scripts/JakesSBSVLC.cs
+++ Assets/JakeDowns/Scripts/JakesSBSVLC.cs
@@ -569,7 +569,25 @@ public class JakesSBSVLC : MonoBehaviour
             }
         }, fileTypes);
 
+        _ShowAndroidToastMessage($"Permission result: {permission}");
         Debug.Log("Permission result: " + permission);
+    }
+
+    /// <param name="message">Message string to show in the toast.</param>
+    private void _ShowAndroidToastMessage(string message)
+    {
+        AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject unityActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+
+        if (unityActivity != null)
+        {
+            AndroidJavaClass toastClass = new AndroidJavaClass("android.widget.Toast");
+            unityActivity.Call("runOnUiThread", new AndroidJavaRunnable(() =>
+            {
+                AndroidJavaObject toastObject = toastClass.CallStatic<AndroidJavaObject>("makeText", unityActivity, message, 0);
+                toastObject.Call("show");
+            }));
+        }
     }
 
     void Log(object message)
